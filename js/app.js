efe7e6ab336d6deca1fc7fe836e4d63cd94fb459
Samuel Olagunju <plextoraa.a@gmail.com>
2021-11-12 19:07:38 -0500

    Added CSS, Pitch changer, and removed pause button

--- js/app.js
+++ js/app.js
@@ -1,14 +1,13 @@
 const playButton = document.getElementById('play-button')
-const pauseButton = document.getElementById('pause-button')
 const stopButton = document.getElementById('stop-button')
 const textInput = document.getElementById('text')
 const speedInput = document.getElementById('speed')
+var pitchInput = document.getElementById('pitch');
 let currentCharacter
 
 playButton.addEventListener('click', () => {
   playText(textInput.value)
 })
-pauseButton.addEventListener('click', pauseText)
 stopButton.addEventListener('click', stopText)
 speedInput.addEventListener('input', () => {
   stopText()
@@ -30,12 +29,9 @@ function playText(text) {
   if (speechSynthesis.speaking) return
   utterance.text = text
   utterance.rate = speedInput.value || 1
+  utterance.pitch = pitchInput.value || 1
   textInput.disabled = true
   speechSynthesis.speak(utterance)
-}
-
-function pauseText() {
-  if (speechSynthesis.speaking) speechSynthesis.pause()
 }
 
 function stopText() {
